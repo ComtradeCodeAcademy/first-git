d1a32e71ad4098617c6bfcb4e661c22c93d9e9c9
pedja <designbyheart@gmail.com>
2018-04-10 18:54:25 +0200

    first commit

---
+++ Lesson 10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson 10
+//
+//  Created by Predrag Jevtic on 4/10/18.
+//  Copyright © 2018 com.comtrade.CodeAcademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
